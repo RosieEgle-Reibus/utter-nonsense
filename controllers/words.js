5d2f6c2f1e18281b2b28b1aa7c80ca357584ca63
Rose Egle <rosiemegle89@gmail.com>
2019-10-20 16:58:47 -0400

    made controllers for wordsSchema

---
+++ controllers/words.js
@@ -0,0 +1,71 @@
+
+const express = require('express')
+
+const wordsApi = require('../models/words.js')
+
+const wordsRouter = express.Router()
+
+//getAll
+wordsRouter.get('/word', (req, res) => {
+  wordsApi.getAllWords()
+  .then((allWords) => {
+    res.json(allWords)
+  })
+})
+
+//getOne
+wordsRouter.get('/word/:id', (req, res) => {
+  wordsApi.getOneWord(req.params.id)
+  .then((singleWord) => {
+    res.json(singleWord)
+  })
+})
+
+//createOne 
+wordsRouter.post('/word/new', (req, res) => {
+  wordsApi.createWord(req.body)
+  .then((createdWord) => {
+    res.json(createdWord)
+  })
+})
+
+//updateOne
+wordsRouter.put('/word/edit/:id', (req, res) => {
+  wordsApi.updateWord(req.params.id, req.body)
+  .then((updatedWord) => {
+    res.json(updatedWord)
+  })
+})
+
+//deleteOne
+wordsRouter.delete('/word/:id', (req, res) => {
+  wordsApi.deleteWord(req.params.id)
+  .then((deletedWord) => {
+    res.json(deletedWord)
+  })
+})
+
+module.exports = {
+  wordsRouter
+}
+
+// Words to place in the paragraph/story
+// const wordsSchema {
+//   mammal: String,
+//   object: String,
+//   verb: String,
+//   adjective: String,
+//   number: Number
+// }
+// Pictures- the user will paste the url in and the images will become the illustrations for the story
+// const pictureSchema {
+//   destination: String,
+//   mammal: String,
+//   object: String,
+//   dateTaken: Date,
+// }
+// Theme will be a drop down that will select between three different stories/paragraphs to insert the above data into
+// const themeSchema {
+//   type: String,
+//   enum: ['Nature', 'Breakfast', 'Murder-y' ]
+// }
