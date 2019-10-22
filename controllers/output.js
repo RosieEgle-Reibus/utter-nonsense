685cdc0f268683e22fb8f28e7c8ee7469fcb5277
Rose Egle <rosiemegle89@gmail.com>
2019-10-22 15:30:41 -0400

    added views for all theme options and created new controllers to get each one

--- controllers/output.js
+++ controllers/output.js
@@ -28,7 +28,7 @@ const outputRouter = express.Router()
 //         .then((allPictures) => {
 //             res.json(allPictures)
 //         })
-    
+
 // })
 // outputRouter.get('/output', (req, res) => {
 //     Promise.all([
@@ -69,41 +69,81 @@ const outputRouter = express.Router()
 //     })
 // })
 
-// outputRouter.get('/', (req, res) => {
-//     res.render('output/createAllSchemasForm')
-// })
+//get master create form
+outputRouter.get('/', (req, res) => {
+    res.render('output/createAllSchemasForm')
+})
 
-outputRouter.get('/output', (req, res) => {
+//get the murder theme output
+outputRouter.get('/output/murder', (req, res) => {
     Promise.all([
         wordsApi.getAllWords(),
         picturesApi.getAllPictures()
     ])
-    .then(([allWords, allPictures]) => {
-        //console.log( allWords, allPictures)
-        res.render('output/output', {allWords, allPictures})
-    })
+        .then(([allWords, allPictures]) => {
+            //console.log( allWords, allPictures)
+            res.render('output/murder', { allWords, allPictures })
+        })
 })
 
-// outputRouter.post('/output', (req, res) => {
-//     Promise.all([
-//         wordsApi.createWord(req.body),
-//         picturesApi.createPicture(req.body),
-//         themesApi.createTheme(req.body)
-//     ])
-//     .then(([createdWord, createdPicture, createdTheme]) => {
-//         res.redirect('/output')
-//     })
-// })
+//get the nature theme output
+outputRouter.get('/output/nature', (req, res) => {
+    Promise.all([
+        wordsApi.getAllWords(),
+        picturesApi.getAllPictures()
+    ])
+        .then(([allWords, allPictures]) => {
+            //console.log( allWords, allPictures)
+            res.render('output/nature', { allWords, allPictures })
+        })
+})
 
+//get the breakfast theme output
+outputRouter.get('/output/breakfast', (req, res) => {
+    Promise.all([
+        wordsApi.getAllWords(),
+        picturesApi.getAllPictures()
+    ])
+        .then(([allWords, allPictures]) => {
+            //console.log( allWords, allPictures)
+            res.render('output/breakfast', { allWords, allPictures })
+        })
+})
 
+outputRouter.get('/output/writein', (req, res) => {
+    Promise.all([
+        wordsApi.getAllWords(),
+        picturesApi.getAllPictures(),
+        themesApi.getAllThemes()
+    ])
+        .then(([allWords, allPictures, allThemes]) => {
+            //console.log( allWords, allPictures)
+            res.render('output/writeInTheme', { allWords, allPictures, allThemes })
+        })
+})
 
+//create ALL schemas with master form
+outputRouter.post('/output', (req, res) => {
+    Promise.all([
+        wordsApi.createWord(req.body),
+        picturesApi.createPicture(req.body),
+        themesApi.createTheme(req.body)
+    ])
+        .then(([createdWord, createdPicture, createdTheme]) => {
+            if (createdTheme.dropDownTheme === "murder-y") {
+                res.redirect('/output/murder')
+            } else if (createdTheme.dropDownTheme === "nature") {
+                res.redirect('/output/nature')
+            } else if (createdTheme.dropDownTheme === "breakfast") {
+                res.redirect('/output/breakfast')
+            } else {
+                res.redirect('/output/writein')
+            }
 
-
-
-
-
+        })
+})
 
 
 module.exports = {
     outputRouter
-  }
+}
